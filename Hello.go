3d9ec3f4c11e734f2b14cca8e8770154a0f765ae
Razarx02 <specialforalem@mail.ru>
2021-12-26 18:32:11 +0600

    Hello world

---
+++ Hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// import "fmt"
+
+func main() {
+	var let int
+	let = 45
+
+	str := "Hello world"
+	text := "My name is "
+	name := "Dias"
+	fmt.Println(str)
+	fmt.Println(let)
+	fmt.Println(text + name)
+}
