1a8f5b744f36dc8ca0caae8a0d61bc52dd4db94a
Razarx02 <specialforalem@mail.ru>
2021-12-27 02:39:35 +0600

    loop number 1

---
+++ loops.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For can worl Like while
+	counter := 0
+
+	for {
+		if counter > 10 {
+			return
+		} else {
+			counter++
+		}
+		fmt.Print(counter)
+	}
+
+	
+
+}
