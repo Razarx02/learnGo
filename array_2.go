8b30efcb8da3fb64bbcc89e743bb82938923bd57
Razarx02 <specialforalem@mail.ru>
2021-12-28 02:28:15 +0600

    array in golang

---
+++ array_2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	array := []string{"hello world", "i am admin"}
+	fmt.Println(array)
+	text := "Hello world"
+	fmt.Println(len(array))
+	fmt.Println(len(text))
+	
+}
