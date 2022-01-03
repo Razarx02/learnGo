af94cc2cc46ad428a18c7971472918715b9164c9
Razarx02 <specialforalem@mail.ru>
2022-01-03 16:39:44 +0600

    Easy level

--- leetcode/array/maximumWealth.go
+++ leetcode/array/maximumWealth.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// Input: accounts = [[1,2,3],[3,2,1]]
+// Output: 6
+// Explanation:
+// 1st customer has wealth = 1 + 2 + 3 = 6
+// 2nd customer has wealth = 3 + 2 + 1 = 6
+// Both customers are considered the richest with a wealth of 6 each, so return 6
 
 func main() {
 	array := [][]int{{1,5},{7,3},{3,5}}
